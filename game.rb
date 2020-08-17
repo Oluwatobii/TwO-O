7ee0723db56d40858650e0b77b3df7b86768df52
Oluwatobii <bellotobi10@gmail.com>
2020-08-17 17:42:37 +0000

    Creating classes and initializing it

---
+++ game.rb
@@ -0,0 +1,43 @@
+require_relative './player'
+require_relative './question'
+
+class Game 
+
+  # Constructor
+  def initialize
+
+    # Creating the players
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+    @players = [@player1, @player2]
+
+    # No. of rounds
+    @round = 1
+  end
+
+  # No. of Rounds
+  
+
+  # Game Status
+  
+
+  # Get an array of remaining players
+  
+
+  # Get the winner
+  
+  
+  # Check if it's game over (Lost all three lives)
+  
+
+  
+  # Method to check answer
+  
+  
+  # Play method - Main game loop
+  
+    # Check who won
+
+
+
+end
---
+++ player.rb
@@ -0,0 +1,18 @@
+class Player
+  attr_reader :name
+  attr_accessor :hp
+
+  def initialize(name)
+    # An instance variable. Meaning each object comes with this
+    @name = name
+    @hp = 3
+  end
+
+  # Check if player is dead
+ 
+
+  # Deduct life point if player gave a wrong answer
+
+
+
+end
